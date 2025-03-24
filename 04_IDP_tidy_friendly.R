dec24957471996b7453be27a7e1c2a45b8d4e52a
Fanny Franchini <fannyfranchini@gmail.com>
2025-03-24 17:27:09 +1100

    tidy implementation file and fail

---
+++ 04_IDP_tidy_friendly.R
@@ -0,0 +1,13 @@
+# ------------------------------------- #
+# Tidyverse-friendly IDP implementation #
+# ------------------------------------- #
+
+## Libraries
+library(tidyverse)
+library(lubridate)
+
+## Set seed for reproducibility
+set.seed(239)
+
+## this is not completed and is more complicated to implement than I thought :)
+
